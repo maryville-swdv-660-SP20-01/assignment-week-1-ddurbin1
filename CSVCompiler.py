1e9855d22f78a7dcf06522b575455dc9fa43d95c
ddurbin1 <35539466+ddurbin1@users.noreply.github.com>
2020-01-14 11:44:23 -0600

    File creation iteration 1
    First file creation of CSVCompiler

---
+++ CSVCompiler.py
@@ -0,0 +1,25 @@
+import os
+import glob
+import pandas as pd
+from colorama import init, Fore, Style
+
+def main():
+    style = Style.BRIGHT
+    color = Fore.GREEN
+
+    user_input = input('Enter file directory: ')
+
+    os.chdir(user_input)
+
+    extension = 'csv'
+    all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+    #combine all files in the list
+    combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+    #export to csv
+    combined_csv.to_csv("combined_csv.csv", index = False, encoding = 'utf-8-sig')
+
+    print(color + style + 'Finished combining files...')
+    
+if __name__ == '__main__': 
+    main()
