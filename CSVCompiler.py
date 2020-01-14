0d29815d1a83bb1c8af80179888919f693a40b54
Devin Durbin <35539466+ddurbin1@users.noreply.github.com>
2020-01-14 11:07:29 -0600

    Add files via upload

---
+++ CSVCompiler.py
@@ -0,0 +1,18 @@
+import os
+import glob
+import pandas as pd
+from more_itertools import unique_everseen
+
+user_input = input('Enter file directory: ')
+
+os.chdir(user_input)
+
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+#combine all files in the list
+combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames])
+#export to csv
+combined_csv.to_csv("combined_csv.csv", index = False, encoding = 'utf-8-sig')
+
+print("Finished combining files...")
